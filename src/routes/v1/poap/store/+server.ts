25d35dcaa460876154a7f8ae9c268e2e450deba7
juanm95 <juanmarroquin95@gmail.com>
2023-05-24 14:04:38 -0700

    Adding default imageUrl

--- src/routes/v1/poap/store/+server.ts
+++ src/routes/v1/poap/store/+server.ts
@@ -2,15 +2,19 @@ import { json } from "@sveltejs/kit";
 import { PrismaDatabase } from '$lib/prismaDatabase';
 
 const prismaDatabase = new PrismaDatabase();
+const defaultImageUrl = "https://cdn-images-1.medium.com/max/800/1*5FNuSa1b6gVd70NmZQmu7Q.png";
 
 export async function POST({ request }) {
     try {
         const tokenMetadata = await request.json() ?? {};
         // sanitize
-        if (!tokenMetadata.name || !tokenMetadata.description || !tokenMetadata.imageUrl) {
+        if (!tokenMetadata.name || !tokenMetadata.description) {
             return json({ error: "Invalid input" }, { status: 400 });
         }
-        const claimCode = await prismaDatabase.createToken(tokenMetadata);
+        const claimCode = await prismaDatabase.createToken({
+            ...tokenMetadata,
+            imageUrl: tokenMetadata.imageUrl || defaultImageUrl
+        });
         return json({
             claimCode,
             claimUrl: `http://${request.headers.get("host")}?id=${claimCode}`
