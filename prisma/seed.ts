b3934d48a1de2785fa53e257c271a50cdba301fc
juanm95 <juanmarroquin95@gmail.com>
2023-05-31 11:42:33 -0700

    adding creator

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,12 +7,10 @@ const tokens: Prisma.TokenCreateInput[] = [
         name: 'EOS Network Fireside Chat Token',
         description: 'Token for attending a fireside chat.',
         imageUrl: 'https://cdn-images-1.medium.com/max/800/1*5FNuSa1b6gVd70NmZQmu7Q.png',
-        users: {
-            create: [
-                {
-                    address: '0xf4C31FA6605a233259a6D8f8762da1E136131784',
-                },
-            ],
+        creator: {
+            create: {
+                address: '0xf4C31FA6605a233259a6D8f8762da1E136131784',
+            },
         },
     },
     {
@@ -20,6 +18,11 @@ const tokens: Prisma.TokenCreateInput[] = [
         description: 'Token granted to attendees of the monthly dog lovers meeting',
         imageUrl:
             'https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+        creator: {
+            create: {
+                address: '0xf4C31FA6605a233259a6D8f8762da1E136131784',
+            },
+        },
     },
 ];
 
