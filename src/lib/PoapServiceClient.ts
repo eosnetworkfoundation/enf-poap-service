a578618bbfc316244c67848104e95341d0d4ceb9
Kelly Tran <43912390+kellytran3k@users.noreply.github.com>
2023-06-07 11:54:48 -0400

    Added Preview Page for Token with DB read/write (#14)
    * added preview page with DB read/write
    
    * prettier
    
    * remove comments

--- src/lib/PoapServiceClient.ts
+++ src/lib/PoapServiceClient.ts
@@ -1,4 +1,5 @@
-import type { Token } from "@prisma/client";
+import type { Token } from '@prisma/client';
+import Token from '../components/Token.svelte';
 
 export class PoapServiceClient {
     fetch: typeof fetch;
@@ -7,6 +8,26 @@ export class PoapServiceClient {
     constructor(origin?: string, fetchImpl?: typeof fetch) {
         this.origin = origin || (location && location.origin);
         this.fetch = fetchImpl || fetch;
+    }
+
+    async getToken(claimCode: string): Promise<Token[]> {
+        try {
+            const response = await this.fetch(`${this.origin}/v1/poap`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify([claimCode]),
+            });
+
+            if (!response.ok) {
+                return [];
+            }
+            return await response.json();
+        } catch (error) {
+            console.error(error);
+            return [];
+        }
     }
 
     async getClaimedTokens(address: string): Promise<Token[]> {
@@ -41,6 +62,30 @@ export class PoapServiceClient {
                 },
                 body: JSON.stringify({
                     address,
+                }),
+            });
+
+            if (!response.ok) {
+                return [];
+            }
+
+            return await response.json();
+        } catch (error) {
+            console.error(error);
+            return [];
+        }
+    }
+
+    async claimToken(address: string, claimCode: string): Promise<Token[]> {
+        try {
+            const response = await this.fetch(`${this.origin}/v1/poap/claim`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    address,
+                    claimCode,
                 }),
             });
 
