a4d65497db84fa5f1d14894844ffa340fcc6ab9a
juanm95 <juanmarroquin95@gmail.com>
2023-06-08 16:49:26 -0700

    Revert "prettier"
    This reverts commit 6341be0f967ea1be99172481d1f7a0105571b8a8.

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -1,4 +1,5 @@
 import { PUBLIC_CONTRACT_ADDRESS } from '$env/static/public';
+import.meta.env.CONTRACT_ADDRESS;
 
 // add any env vars needed in client here for use
 export const APP_CONFIG = async () => {
